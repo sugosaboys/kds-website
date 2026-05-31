d592c3e5c4950c1273de93dd77f4fa0c06af613e
sugosaboys <lolaloliandecalolaloli@gmail.com>
2026-06-01 07:22:09 +0800

    prerender fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,9 +18,9 @@ export default defineNuxtConfig({
     }
   },
   ssr:true,
-  nitro:{
-    prerender:{
-      routes: ['/'],
-    }
-  }
+  // nitro:{
+  //   prerender:{
+  //     routes: ['/'],
+  //   }
+  // }
 })
